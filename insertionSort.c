394d7f0df1b04567ff3bf34a5efdf43a26291b77
Walter Johnson <johnwalt@oregonstate.edu>
2020-10-04 00:17:02 -0700

    changed another counter indeclarations for OSU's C version

--- insertionSort.c
+++ insertionSort.c
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 
     int i = 0;
     int j = -7;
-    for (int j = 1; j < arrayLength; j++)
+    for (j = 1; j < arrayLength; j++)
     {
         int key = A[j];
         i = j - 1;
