6dfc5853ad492322c2ed88150f2b8f7c1da712c6
Walter Johnson <johnwalt@oregonstate.edu>
2020-11-30 21:05:56 -0800

    forgotten comments

--- fiboSteps.c
+++ fiboSteps.c
@@ -4,30 +4,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long int getWays(int A);
+unsigned long long int getWays(int A);
 
 int main(int argc, char* argv[])
 {
-    int A = 7; // can't be larger than 47
+    int A = 13; // can't be larger than 91
     if (argc == 2)
     {
         A = atoi(argv[1]);
     }
 
-    unsigned long int distinctWaysToA = getWays(A);
+    unsigned long long int distinctWaysToA = getWays(A);
 
     printf("Given %d steps, there are %lld distinct ways to climb to the top.\n", A, distinctWaysToA);
 }
 
-unsigned long int getWays(int A)
+unsigned long long int getWays(int A)
 {
-    unsigned long int previousTwo[] = {1, 1}; // base cases: steps 0 and 1
+    unsigned long long int previousTwo[] = {1, 1}; // base cases: steps 0 and 1
 
-    int index = 1;
+    int index = 1; // which previousTwo are we modifying
     int j = -7;
-    for (j = 2; j <= A; j++)
+    for (j = 2; j <= A; j++)  // step 2 through step A
     {
-        index = index == 0 ? 1 : 0;
+        index = index == 0 ? 1 : 0;  // switch back and forth in previousTwo to store current
         previousTwo[index] = previousTwo[0] + previousTwo[1];
     }
 
